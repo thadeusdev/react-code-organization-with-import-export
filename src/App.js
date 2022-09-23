9c3b4d77226d0932b5cc948533362942429676ff
thadeusdev <109853680+thadeusdev@users.noreply.github.com>
2022-09-23 19:51:11 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Header from './Header';
+import Card from './Card'
+import EmojiButton from './EmojiButton';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Card title="Welcome to PetLand!">
+          <em>Find your dream pet</em>
+        </Card>
+        <Card title="What pets would you like to see?">
+          <div>
+            <EmojiButton emoji="😸" label="Cats" />
+            <EmojiButton emoji="🐕" label="Dogs" />
+          </div>
+        </Card>
+      </main>
+    </div>
+  );
+}
+
+export default App
